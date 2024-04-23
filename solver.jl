c66b99bfe79d6c6044a365b96ef5baea3b196b15
KaiJyunWang <b11303072@ntu.edu.tw>
2024-04-23 16:28:26 +0800

    remove paper

--- solver.jl
+++ solver.jl
@@ -1,6 +1,6 @@
 using Distributions, LinearAlgebra, Plots, Interpolations
 using Parameters, Random, Tables, Profile
-using CUDA, CUDAKernels, KernelAbstractions, Tullio
+using CUDA, CUDAKernels, KernelAbstractions, Tullio, Adapt
 using BenchmarkTools
 
 #life-cycle problem of pension solver
@@ -28,20 +28,12 @@ end
 
 mort = mortality([1.5, 100000.0, 0.1, 0.0003])
 
+#profile of aime
 profile = [2.747, 3.48, 4.58]
-function aime(rame::Float64, profile::Array{Float64,1} = profile)
-    if rame < profile[2]
-        return profile[1]
-    elseif rame < profile[3]
-        return profile[2]
-    else
-        return profile[3]
-    end
-end
-plot(1.0:0.02:5.0, x -> aime(x), label = "aime")
+
 #pension benefit formula
 #unit: month wage
-#pension type: 0: no pension, 1: once pension, 2: monthly pension
+#pension type: 1: no pension, 2: lump-sum pension, 3: monthly pension, 4: received pension
 function benefit(aime::Float64, p::Int64, h::Int64, t::Int64, ra::Int64)
     if (p == 1) || (p == 4)
         return 0.0
@@ -51,16 +43,14 @@ function benefit(aime::Float64, p::Int64, h::Int64, t::Int64, ra::Int64)
         return max(h*aime*0.00775+3, h*aime*0.0155)*(1+0.04*min(abs(t-ra), 5)*sign(t-ra))
     end
 end
-plot!(1.0:0.02:5.0, x -> benefit(aime(x), 2, 20, 63, 60), label = "benefit")
 #pension tax formula
 function tax(aime::Float64, p::Int, τ::Float64 = 0.12)
-    if p == 0
+    if p == 1
         return aime*0.2*τ
     else
         return 0.0
     end
 end
-plot!(1.0:0.02:5.0, x -> tax(aime(x), 0), label = "tax")
 #utility function 
 function u(c::Float64, n::Int64; γ::Float64, η::Float64, c_min::Float64)
     if c ≥ c_min
@@ -74,79 +64,117 @@ function u(c::Float64, n::Int64; γ::Float64, η::Float64, c_min::Float64)
     end
 end
 
-para = @with_kw (γ = 1.5, η = 0.6, r = 0.02, β = 1/(1+r), ϵ = collect(range(0.0, 1.0, 5)), 
-    T = 100, μ = mort.μ, init_t = 40, ρ = 0.97, σ = 0.1, ξ = σ*randn(250), a = collect(range(0.0, 25.0, 15)), 
-    n = [0,1], h = collect(0:20), h_ceil = 10, c_min = 0.1, δ = [0.1, -0.003, 0.0], φ_l = 1.0, θ_b = 0.5, κ = 2.0,
-    aime = profile, p = collect(1:4), ra = 60, τ = 0.12, lme = profile)
+function stair(x::Float64; c::Array{Float64,1} = c)
+    if x < c[2]
+        return c[1]
+    elseif x < c[3]
+        return c[2]
+    else
+        return c[3]
+    end
+end
+
+para = @with_kw (γ = 1.5, η = 0.6, r = 0.02, β = 1/(1+r), ϵ = collect(range(0.0, 2.0, 11)), 
+    T = 80, μ = mort.μ, init_t = 40, ρ = 0.97, σ = 0.1, ξ = σ*randn(250), asset = collect(range(0.0, 25.0, 26)), 
+    work = [0,1], wy = collect(0:30), wy_ceil = 30, c_min = 0.1, δ = [0.05, -0.004], φ_l = 0.5, θ_b = 0.0, κ = 2.0,
+    aime = profile, plan = collect(1:4), ra = 60, τ = 0.12, lme = profile)
 para = para()
-v = zeros(length(para.a), length(para.ϵ), length(para.h), length(para.n), length(para.aime), length(para.p), length(para.lme), para.T-para.init_t+2)
+
+v = zeros(length(para.asset), length(para.ϵ), length(para.wy), length(para.plan), length(para.work), length(para.aime), length(para.lme), para.T-para.init_t+2)
 function solve(v::Array{Float64,8};para)
-    (; γ, η, r, β, ϵ, T, μ, init_t, ρ, σ, ξ, a, n, h, h_ceil, c_min, δ, φ_l, θ_b, κ, aime, p, ra, τ, lme) = para
-    
-    h_comp = [δ[1]*min(h_ceil, h[m]) + δ[2]*min(h_ceil, h[m])^2 for m in 1:length(h)] |> x -> CuArray(x)
+    (; γ, η, r, β, ϵ, T, μ, init_t, ρ, σ, ξ, asset, work, wy, wy_ceil, c_min, δ, φ_l, θ_b, κ, aime, plan, ra, τ, lme) = para
 
+    wy_comp = [δ[1]*min(wy_ceil, wy[m]) + δ[2]*min(wy_ceil, wy[m])^2 for m in 1:length(wy)] |> x -> CuArray(x)
 
-    candidate = zeros(length(a), length(ϵ), length(h), length(n), length(aime), length(p), length(lme), length(a), length(n), length(p)) |> x -> CuArray(x)
-    current_wage = zeros(length(ϵ), length(h)) |> x -> CuArray(x)
-    pension_tax = zeros(length(ϵ), length(h), length(p)) |> x -> CuArray(x)
-    pb = zeros(length(h), length(aime), length(p)) |> x -> CuArray(x)
-    pension_allow = zeros(length(p), length(p)) |> x -> CuArray(x)
-    rpb = zeros(length(h), length(aime), length(p), length(p)) |> x -> CuArray(x)
-    consumption = zeros(length(a), length(a), length(n), length(ϵ), length(h), length(n), length(aime), length(p), length(p)) |> x -> CuArray(x)
-    flow_utility = zeros(length(a), length(a), length(n), length(ϵ), length(h), length(n), length(aime), length(p), length(p)) |> x -> CuArray(x)
-    bequest = zeros(length(a)) |> x -> CuArray(x)
-    future_aime = zeros(length(n), length(ϵ), length(h), length(aime), length(lme)) |> x -> CuArray(x)
-    future_lme = zeros(length(ϵ), length(h), length(lme)) |> x -> CuArray(x)
-    future_utility = zeros(length(a), length(n), length(ϵ), length(h), length(aime), length(p), length(lme)) |> x -> CuArray(x)
-    future_value = zeros(length(a), length(n), length(ϵ), length(h), length(aime), length(p), length(lme)) |> x -> CuArray(x)
-    pension_tr = zeros(length(p), length(p)) |> x -> CuArray(x)
-
-    policy = zeros(length(a), length(ϵ), length(h), length(n), length(aime), length(p), length(lme), T-init_t+1, 3) |> x -> CuArray(x)
-
-
+    #policy function 1: asset, 2: work, 3: plan
+    policy = zeros(length(asset), length(ϵ), length(wy), length(plan), length(work), length(aime), length(lme), T-init_t+1, 3)
+    #computing matrices
+    lumpsum_benefit = zeros(length(aime), length(wy), length(plan))
+    monthly_benefit = zeros(length(aime), length(wy), length(plan))
+    benefit = zeros(length(aime), length(wy), length(plan))
+    adj_cost = zeros(length(work), length(work))
+    f_ϵ = zeros(length(ϵ))
+    wage = zeros(length(ϵ), length(wy))
+    c_aime = zeros(length(ϵ), length(wy))
+    pension_tax = zeros(length(ϵ), length(wy))
+    income = zeros(length(ϵ), length(wy), length(work), length(plan), length(aime))
+    consumption = zeros(length(asset), length(asset), length(ϵ), length(wy), length(work), length(work), length(plan), length(aime))
+    leisure = zeros(length(work))
+    utility = zeros(length(asset), length(asset), length(work), length(ϵ), length(wy), length(work), length(plan), length(aime))
+    bequest = zeros(length(asset))
+    forbid = zeros(length(plan), length(plan), length(wy))
+    f_lme = zeros(length(lme), length(ϵ), length(wy), length(work))
+    f_wy = zeros(length(wy), length(work))
+    less_5y_f_aime = zeros(length(aime), length(ϵ), length(wy), length(work))
+    more_5y_f_aime = zeros(length(aime), length(lme), length(ϵ), length(wy), length(work))
+    f_aime = zeros(length(aime), length(lme), length(ϵ), length(wy), length(work))
+    f_utility = zeros(length(asset), length(work), length(ϵ), length(wy), length(aime), length(lme), length(plan))
+    candidate = zeros(length(asset), length(ϵ), length(wy), length(plan), length(work), length(aime), length(lme), length(asset), length(work), length(plan))
 
     ϵ_grid = zeros(length(ϵ), T-init_t+2)
     ϵ_grid[:,1] = ϵ
     for t in 2:T-init_t+2
         ϵ_grid[:,t] = range(ρ*ϵ_grid[1,t-1] + minimum(ξ), ρ*ϵ_grid[end,t-1] + maximum(ξ), length(ϵ))
     end
-    
-    a_cua = CuArray(a)
-    n_cua = CuArray(n)
-    
-    @tullio pension_allow[z,f] = ((p[z] != 1) ? ((((p[z] == 2)||(p[z] == 4)) ? 0 : (((p[z] == 3)&&(p[f] == 3)) ? 1 : 0))) : 1)
-    @tullio bequest[j] = $θ_b*(($κ + a_cua[j])^(1 - $γ))/(1 - $γ)
-    @tullio pension_tr[z,f] = ((p[f] == 1)||((p[f] == 3)&&(p[z] == 3))||((p[z] == 2)&&(p[z] == 4))||((p[z] == 4)&&(p[z] == 4))) ? 0 : -1e200
+
+    #benefit schemes
+    #2: lump-sum pension, 3: monthly pension
+    @tullio lumpsum_benefit[y,m,q] = (plan[q] == 2)*min(max(wy[m], 2*wy[m]-15), 50)*aime[y]
+    @tullio monthly_benefit[y,m,q] = (plan[q] == 3)*max(wy[m]*aime[y]*0.00775+3, wy[m]*aime[y]*0.0155)
+    #adjustment cost of working
+    @tullio adj_cost[k,x] = (1-$φ_l*(work[k]-work[x] == 1))
     
     for s in 1:T-init_t+1
         t = T-s+1
+        extra_benefit = (t-ra) ≤ 5 ? 0.04*(t-ra) : 0.0
         mort = μ[t]
-        v_func = LinearInterpolation((a, ϵ_grid[:,t-init_t+2], h, n, aime, p, lme), v[:,:,:,:,:,:,:,t-init_t+2])
+        v_func = LinearInterpolation((asset, ϵ_grid[:,t-init_t+2], wy, plan, work, aime, lme), v[:,:,:,:,:,:,:,t-init_t+2])
         ϵ_cua = CuArray(ϵ_grid[:,t-init_t+1])
+        
+        #future ϵ, avg. 
+        @tullio f_ϵ[l] = $ρ * ϵ_cua[l]
+        #minimum wage is 2.747
+        @tullio wage[l,m] = max(2.747, exp(ϵ_cua[l] + wy_comp[m]))
+        #current wage transformed into aime
+        @tullio c_aime[l,m] = stair(wage[l,m]; c = aime)
+        @tullio pension_tax[l,m] = c_aime[l,m]*0.2*$τ
+        #benefit
+        @tullio benefit[y,m,q] = lumpsum_benefit[y,m,q] + monthly_benefit[y,m,q]*(1+$extra_benefit)
+        #net income
+        @tullio income[l,m,k,q,y] = wage[l,m]*work[k] + benefit[y,m,q] - pension_tax[l,m]*work[k]*(plan[q] == 1)
+        #consumption
+        @tullio consumption[i,j,l,m,k,x,q,y] = (1+$r)*asset[i] + income[l,m,k,q,y] + adj_cost[k,x] - asset[j]
+        @tullio leisure[k] = 364-260*work[k]
+        #consumption floor
+        @tullio utility[i,j,k,l,m,x,q,y] = consumption[i,j,l,m,k,x,q,y] > $c_min ? ($η*log(consumption[i,j,l,m,k,x,q,y]) + (1-$η)*log(leisure[k]))*(1-$γ) : -1e200
+        #bequest
+        @tullio bequest[j] = $θ_b*($κ+asset[j])^(1-$γ)*$mort
+        #forbidden path of applying for pension
+        #1: unreceived, 2: lump-sum, 3: monthly, 4: received lump-sum
+        @tullio forbid[p,q,m] = (((wy[m] == 0)&&(p != 4))||((plan[p] == 2)&&(plan[q] != 4))||((plan[p] == 3)&&(plan[q] != 3))||((plan[p] == 4)&&(plan[q] != 4)) ? -1e200 : 0.0)
+        #future lowest monthly wage
+        @tullio f_lme[z,l,m,k] = min(4.58, max(lme[z], wage[l,m]*work[k]))
+        #future work years
+        @tullio f_wy[m,k] = min(wy[m]+work[k], $wy_ceil)
+        #future aime
+        @tullio less_5y_f_aime[y,l,m,k] = c_aime[l,m]*work[k]/(wy[m]+work[k]) + wy[m]/(wy[m]+work[k])*aime[y]
+        @tullio more_5y_f_aime[y,z,l,m,k] = aime[y] + 0.2*max(0, c_aime[l,m]*work[k]-lme[z])
+        @tullio f_aime[y,z,l,m,k] = (((wy[m] + work[k]) > 0) ? (wy[m] < 5 ? less_5y_f_aime[y,l,m,k] : more_5y_f_aime[y,z,l,m,k]) : 2.747)
+        @tullio f_aime[y,z,l,m,k] = min(4.58, f_aime[y,z,l,m,k])
+        #EV part
+        @tullio f_utility[j,k,l,m,y,z,q] = mean(v_func.(asset[j], f_ϵ[l] .+ ξ, f_wy[m,k], plan[q], work[k], f_aime[y,z,l,m,k], f_lme[z,l,m,k]))
 
-        #minimum wage
-        @tullio current_wage[l,m] = max(2.747, exp(ϵ_cua[l] + h_comp[m]))
-        @tullio pension_tax[l,m,f] = (0.2*$τ*(p[f] == 1) + !(p[f] == 1))*current_wage[l,m]
-        @tullio pb[m,y,f] = benefit(aime[y], p[f], h[m], $t, $ra)
-        @tullio rpb[m,y,z,f] = pb[m,y,f]*pension_allow[z,f]
-        @tullio consumption[i,j,k,l,m,x,y,z,f] = (1+$r)*a_cua[i] + current_wage[l,m] * n_cua[k] - a_cua[j] + rpb[m,y,z,f] - $φ_l * (1 - n_cua[x]) * n_cua[k] - pension_tax[l,m,f] 
-        @tullio flow_utility[i,j,k,l,m,x,y,z,f] = ((consumption[i,j,k,l,m,x,y,z,f] ≥ $c_min) ? (((consumption[i,j,k,l,m,x,y,z,f])^($η)*(104*n_cua[k] + 364*(1 - n_cua[k]))^(1 - $η))^(1 - $γ))/(1 - $γ) : -1e200)
-        @tullio future_aime[k,l,m,y,g] = min(4.58, ((h[m] != 0) ? ((h[m] < 5) ? (h[m]*aime[y] + current_wage[l,m]*n_cua[k])/(h[m] + n_cua[k]) : (aime[y] + 0.2 * max(0, current_wage[l,m]*n_cua[k] - lme[g]))) : aime[y]))
-        @tullio future_lme[l,m,g] = min(4.58, max(lme[g], current_wage[l,m]))
-        @tullio future_utility[j,k,l,m,y,f,g] = mean(v_func(a_cua[j], $ρ * ϵ_cua[l] .+ ξ, min(h[m] + 1, $h_ceil), n_cua[k], future_aime[k,l,m,y,g], p[f], future_lme[l,m,g]))
-        @tullio future_value[j,k,l,m,y,f,g] = $β * (1 - $mort) * future_utility[j,k,l,m,y,f,g] + $β * $mort * bequest[j]
-
-        @tullio candidate[i,l,m,x,y,z,g,j,k,f] = flow_utility[i,j,k,l,m,x,y,z,f] + future_value[j,k,l,m,y,f,g]
-        @tullio candidate[i,l,m,x,y,z,g,j,k,f] = candidate[i,l,m,x,y,z,g,j,k,f] + pension_tr[z,f]
- 
-
+        @tullio candidate[i,l,m,p,x,y,z,j,k,q] = utility[i,j,k,l,m,x,q,y] + (1-$mort)*$β*f_utility[j,k,l,m,y,z,q] + bequest[j] + forbid[p,q,m]
+        
+        #transform back to CPU
         can = Array(candidate)
         
         for state in CartesianIndices(v[:,:,:,:,:,:,:,t-init_t+1])
             ind = argmax(can[state,:,:,:])
-            policy[state,t-init_t+1,1] = a[ind[1]]
-            policy[state,t-init_t+1,2] = n[ind[2]]
-            policy[state,t-init_t+1,3] = p[ind[3]]
+            policy[state,t-init_t+1,1] = asset[ind[1]]
+            policy[state,t-init_t+1,2] = work[ind[2]]
+            policy[state,t-init_t+1,3] = plan[ind[3]]
+            v[state, t-init_t+1] = can[state,ind]
         end
         
     end
@@ -156,34 +184,60 @@ end
 
 sol = solve(v; para)
 
-plt = plot()
-#draw ar(1) log-wage
-ϵ = fill(0.2, para.T-para.init_t+1)
-h = 0
-h_comp = zeros(para.T-para.init_t+1)
-a = 5.0*ones(para.T-para.init_t+2)
-n = ones(para.T-para.init_t+2)
-sim_aime = 2.747*ones(para.T-para.init_t+1)
-p = ones(para.T-para.init_t+2)
-lme = 2.747*ones(para.T-para.init_t+1)
-
-for t in 2:para.T-para.init_t+2
-    a_func = LinearInterpolation((para.a, sol.ϵ_grid[:,t-1], para.h, para.n, para.aime, para.p, para.lme), sol.policy[:,:,:,:,:,:,:,t-1,1])
-    a[t] = a_func(a[t-1], ϵ[t-1], h, n[t-1], sim_aime[t-1], p[t-1], lme[t-1])
-    n_func = LinearInterpolation((para.a, sol.ϵ_grid[:,t-1], para.h, para.n, para.aime, para.p, para.lme), sol.policy[:,:,:,:,:,:,:,t-1,2])
-    n[t] = floor(Int, n_func(a[t-1], ϵ[t-1], h, n[t-1], sim_aime[t-1], p[t-1], lme[t-1]))
-    p_func = LinearInterpolation((para.a, sol.ϵ_grid[:,t-1], para.h, para.n, para.aime, para.p, para.lme), sol.policy[:,:,:,:,:,:,:,t-1,3])
-    p[t] = floor(Int, p_func(a[t-1], ϵ[t-1], h, n[t-1], sim_aime[t-1], p[t-1], lme[t-1])) #pension type, need more precise
-    h = min(h + n[t],para.h[end])
-    h_comp[t-1] = para.δ[1]*h + para.δ[2]*h^2 
-    ϵ[t-1] = para.ρ*ϵ[t-1] + para.σ*randn()
-    sim_aime[t-1] = min(4.58, ((h != 0) ? ((h < 5) ? (h*2.747 + exp(ϵ[t-1] + h_comp[t-1])*n[t-1])/(h + n[t-1]) : (2.747 + 0.2 * max(0, exp(ϵ[t-1] + h_comp[t-1])*n[t-1] - lme[t-1]))) : 2.747))
-    lme[t-1] = min(4.58, max(lme[t-1], exp(ϵ[t-1] + h_comp[t-1])))
+#simulation
+ϵ = zeros(para.T-para.init_t+1)
+#initial ϵ
+ϵ[1] = 0.7
+for t in 2:para.T-para.init_t+1
+    ϵ[t] = para.ρ*ϵ[t-1] + para.σ*randn()
 end
-plot!(para.init_t:para.T, exp.(ϵ + h_comp), label = "wage")
-plot!(para.init_t:para.T, a[2:end], label = "asset")
-vspan!(para.init_t-1 .+ sort(vcat(findall(x -> x == 1, n[2:end]), findall(x -> x == 1, n[2:end]).+1)) .- 0.5, color = :gray, alpha = :0.4, label = "")
-consumption = (1+para.r)*a[1:end-1] + (exp.(ϵ + h_comp) .* n[2:end]) - a[2:end] .- para.φ_l*max.(0, (n[2:end] - n[1:end-1]))
-findall(x -> x < para.c_min, consumption)
-plot!(para.init_t:para.T, consumption, label = "consumption")
+asset = zeros(para.T-para.init_t+2)
+#initial asset
+asset[1] = 1.0
+work = zeros(para.T-para.init_t+2)
+#initial working status
+work[1] = 1
+wy = zeros(para.T-para.init_t+1)
+#initial work years
+wy[1] = 5
+plan = zeros(para.T-para.init_t+2)
+#initial pension status
+plan[1] = 1
+s_aime = zeros(para.T-para.init_t+1)
+#initial aime
+s_aime[1] = 3.0
+lme = zeros(para.T-para.init_t+1)
+#initial lme
+lme[1] = 2.8
+
+#wage
+wage = zeros(para.T-para.init_t+1)
+consumption = zeros(para.T-para.init_t+1)
+
+for t in 1:para.T-para.init_t
+    a_func = LinearInterpolation((para.asset, sol.ϵ_grid[:,t], para.wy, para.plan, para.work, para.aime, para.lme), sol.policy[:,:,:,:,:,:,:,t,1])
+    w_func = LinearInterpolation((para.asset, sol.ϵ_grid[:,t], para.wy, para.plan, para.work, para.aime, para.lme), sol.policy[:,:,:,:,:,:,:,t,2])
+    p_func = LinearInterpolation((para.asset, sol.ϵ_grid[:,t], para.wy, para.plan, para.work, para.aime, para.lme), sol.policy[:,:,:,:,:,:,:,t,3])
+    asset[t+1] = a_func(asset[t], ϵ[t], wy[t], plan[t], work[t], s_aime[t], lme[t])
+    work[t+1] = round(Int, w_func(asset[t], ϵ[t], wy[t], plan[t], work[t], s_aime[t], lme[t]))
+    plan[t+1] = round(Int, p_func(asset[t], ϵ[t], wy[t], plan[t], work[t], s_aime[t], lme[t]))
+    wage[t] = max(2.747, exp(ϵ[t] + wy[t]))
+    #state transition
+    wy_comp = para.δ[1]*wy[t] + para.δ[2]*wy[t]^2
+    wage[t] = min(exp(ϵ[t] + wy_comp), 2.747)
+    c_aime = stair(wage[t]; c = para.aime)
+    pension_tax = c_aime*0.2*para.τ
+    wy[t+1] = min(para.wy_ceil, wy[t]+work[t+1])
+    s_aime[t+1] = wy[t+1]+work[t+1] > 0 ? (wy[t] < 5 ? c_aime*work[t+1]/(wy[t]+work[t+1]) + wy[t]/(wy[t]+work[t+1])*s_aime[t] : s_aime[t] + 0.2*max(0, c_aime*work[t+1]-lme[t])) : 2.747
+    s_aime[t+1] = min(4.58, s_aime[t+1])
+    lme[t+1] = min(4.58, max(lme[t], wage[t]*work[t+1]))
+    consumption[t] = (1+para.r)*asset[t] + wage[t]*work[t+1] - asset[t+1] + (1-work[t+1])*c_aime*0.2*para.τ*(plan[t+1] == 1) - para.φ_l*(work[t+1]-work[t] == 1)   
+end
+
+#plotting
+age = para.init_t:para.T
+plt = plot(age, wage, label = "wage", xlabel = "age", title = "wage profile")
+plot!(age, asset[2:end], label = "asset")
+vcat(findall(x -> x == 0, work[2:end]) .+ para.init_t .- 0.5, findall(x -> x == 0, work[2:end]) .+ para.init_t .+ 0.5) |> x -> sort(x) |> x -> vspan!(plt, x, color = :gray, label = "retirement", alpha = 0.3)
+
 
